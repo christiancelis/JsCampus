f6b4fef2b5e5ed5a3bdeef8ab1b3d05866393102
christiancelis <73555661+christiancelis@users.noreply.github.com>
2023-12-16 15:49:18 -0500

    finjuegopokemon

--- Batalla-Pokemon/source.js
+++ Batalla-Pokemon/source.js
@@ -54,51 +54,85 @@ function MostrarGanador(id){
     a.style.display = "block"
 }
 
-function InfoAtaque(id){
-    infobatalla = document.getElementById("infob${id")
-    infobatalla.innerHTML = `Pokemon`
+function InfoAtaque(id,str){
+    infobatalla = document.getElementById(`infob${id}`)
+    infobatalla.innerHTML = str
+}
+
+async function Ataque(turno,pokeataque,pokedefensa,idataque,iddefensa){
+    vactual = pokedefensa[3]
+    for(let i=0;i<turno;i++){
+        console.log(`Ataca: pokemon ${idataque}`)
+        console.log("Vida pokemon 1: "+ pokedefensa[3])
+        valorestavida = ((pokedefensa[5]*(1+(pokedefensa[6]/100)))-pokeataque[4]).toFixed(2)
+        if(valorestavida<0){
+            console.log("valor negativo")
+            valorestavida = valorestavida*-1 
+        }
+        pokedefensa[3] = (pokedefensa[3]-valorestavida).toFixed(2)
+        console.log(pokedefensa[3])
+        await delay(5000)
+        CrearElemento(pokedefensa,iddefensa)
+        str =`Turno: ${i+1} <br> 
+        
+        Ataca: pokemon ${idataque} <br>
+        Vida Pokemon  ${iddefensa}: ${vactual}  <br>
+        Valor del Ataque: ${valorestavida} </br>
+        Vida Restante: <span> ${pokedefensa[3]}<span/> </br>` 
+        vactual = vactual - valorestavida
+
+        InfoAtaque(idataque,str)
+        if(pokedefensa[3]<=0){
+            await delay(2000) 
+            MostrarGanador(idataque)
+            return false
+        }
+       
+    }
+
+    return true
+
 }
 
 
 async function Batalla(){
+    console.log(Pokemon1)
+    console.log(Pokemon2)
     do {
-
-    turno = Pokemon1[6]-Pokemon2[6]
-    console.log(turno)
-    if(turno<0){
-        turno = turno * -1
-        for(let i=0;i<turno;i++){
-            Pokemon1[3] = Pokemon1[3]- (Pokemon1[5]*(1+(Pokemon1[6]/100)))-Pokemon2[4]
-            await delay(5000)  
-            CrearElemento(Pokemon1,1)
-            if(Pokemon1[3]<=0){
-                await delay(5000) 
-                MostrarGanador(2)
+        turno = Pokemon1[6]-Pokemon2[6]
+        console.log(turno)
+        if(turno<=0){
+            turno = turno * -1
+            v1 = await Ataque(turno,Pokemon2,Pokemon1,2,1)
+            if(v1===false){
+                return
+            }
+            v2 = await Ataque(1,Pokemon1,Pokemon2,1,2)
+            if(v2===false){
                 return
             }
         }
-
-    }
-    else{
-        Pokemon2[3] = Pokemon2[3]- (Pokemon2[5]*(1+(Pokemon2[6]/100)))-Pokemon1[4]
-        await delay(5000) 
-        CrearElemento(Pokemon2,2)
-
-        if(Pokemon2[3]<=0){
-            await delay(5000) 
-            MostrarGanador(1)
-            return
+        else{
+            v3= await Ataque(turno,Pokemon1,Pokemon2,1,2)
+            if(v3===false){
+                return
+            }
+            v4 = await Ataque(1,Pokemon2,Pokemon1,2,1)
+            if(v4===false){
+                return
+            }
         }
         
-    }    
-    } while (Pokemon1[3]<=0 ||Pokemon1[3]<=0);
-    
 
+    } while (Pokemon1[3]>=0 || Pokemon2[3]>=0);
 }
+
+
 
 function delay(time){
 	return new Promise(resolve => setTimeout(resolve, time));
 }
+
 
 
 function CrearElemento(Pokemon, id){
