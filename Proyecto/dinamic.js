b41fb91aaa3a06ea3b485f93d4c051d143337e2f
christiancelis <73555661+christiancelis@users.noreply.github.com>
2024-01-15 13:47:12 -0500

    proyecto

---
+++ Proyecto/dinamic.js
@@ -0,0 +1,31 @@
+
+ButtonNav = document.querySelector(".menu-hamburguesa")
+
+ButtonNav.addEventListener("click",(e)=>{
+    e.stopPropagation()
+    ImgHamburguesa = document.querySelector("#menu-img")
+    NavElements = document.querySelector(".listaNav")
+   
+    if(NavElements.classList.toggle("visible")==true){
+        ImgHamburguesa.setAttribute("src","./assets/PaginaPrincipal/x-menu-hamburguesa.png")
+    }else{
+        ImgHamburguesa.setAttribute("src","./assets/PaginaPrincipal/menu-hamburguesa.png")
+    }
+})
+
+
+window.addEventListener("scroll",()=>{
+    let reveals = document.querySelectorAll(".reveal")
+    for(let i=0;i<=reveals.length;i++){
+        let windowheigth = window.innerHeight
+        let revealtop = reveals[i].getBoundingClientRect().top 
+        let revealpoint = 80
+
+        if(revealtop<windowheigth-revealpoint){
+            reveals[i].classList.add("active")
+
+        }else{
+            reveals[i].classList.remove("active")
+        }
+    }
+})
